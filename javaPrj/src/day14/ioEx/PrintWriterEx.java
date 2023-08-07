31b381291e4810895ac643cffbc2f2a684f1ed13
jooyeon <victoai999@gmail.com>
2023-08-07 17:57:49 +0900

    15일차

--- javaPrj/src/day14/ioEx/PrintWriterEx.java
+++ javaPrj/src/day14/ioEx/PrintWriterEx.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class PrintWriterEx {
     public static void main(String[] args) {
         try {
-            PrintWriter writer = new PrintWriter("res/numbers.txt");
+            PrintWriter writer = new PrintWriter("res/numbersaaaaaaa.txt");
             
             // 여러 숫자들을 파일에 씁니다.
             int[] numbers = { 10, 20, 30, 40, 50 };
@@ -12,9 +12,12 @@ public class PrintWriterEx {
                 writer.println(number);
             }
             
+            writer.println("안녕세요");
             writer.close(); // 파일 쓰기 종료
             
             System.out.println("숫자들을 파일에 성공적으로 썼습니다.");
+            
+            
         } catch (FileNotFoundException e) {
             System.out.println("파일을 찾을 수 없습니다: " + e.getMessage());
         }
---
+++ javaPrj/src/day16/문자열쓰기.java
@@ -0,0 +1,26 @@
+package day16;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class 문자열쓰기 {
+    public static void main(String[] args) {
+        String content = "안녕하세요";
+
+        try {
+            String filePath = "res/test.txt"; // 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+
+            // 문자열을 파일에 쓰기
+            writer.write(content);
+
+            // 파일 쓰기 완료 후 닫기
+            writer.close();
+            
+            System.out.println("문자열이 파일로 저장되었습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
