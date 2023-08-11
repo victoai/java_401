16d1c8f212c752a5852d73dc1113d23cc19a280a
jooyeon <victoai999@gmail.com>
2023-08-11 17:44:14 +0900

    프로젝트 발표

---
+++ javaPrj/src/day19/Program3.java
@@ -0,0 +1,22 @@
+package day19;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		
+			
+		// 책정보를 정보를 담을 수 있는 변수를 만드는것 
+		// 객체라고 부름   1. ( 구조가 있는 변수 ) + 변수를 다루는 매서드
+		Book book = new Book("자바의정석" ,28000, "도우출판사" );
+		book.매서드( );   // book객체의 정보 전달 됨   => this  참조형변수에 저장됨
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
